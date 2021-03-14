cfa72d101fc11220bf445fe579392c4704cec357
Xheghun <ysenruf@gmail.com>
2021-03-14 18:06:34 +0400

    base ui

---
+++ scripts.js
@@ -0,0 +1,53 @@
+var products = [
+    {
+      "id": 1001,
+      "title": "Siberian Husky",
+      "description": "The Siberian Husky is a medium-sized working sled dog breed. The breed belongs to the Spitz genetic family. It is recognizable by its thickly furred double coat, erect triangular ears, and distinctive markings, and is smaller than the similar-looking Alaskan Malamute.",
+      "price": 241.00,
+      "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fvetwork.co%2Fgalaxy%2Fen%2Fsiberian-husky-dog-breed-pictures-information%2F&psig=AOvVaw2RkjDjqYHxK62DSN8MxVY6&ust=1615813953343000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKiHxp7ur-8CFQAAAAAdAAAAABAD",
+      "availableInventory": 10,
+      "rating": 4
+    },
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "images/2.jpg",
+      "availableInventory": 7,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Kitty Litter",
+      "description": "Premium kitty litter for your cat.",
+      "price": 11.00,
+      "image": "images/3.jpg",
+      "availableInventory": 99,
+      "rating": 4
+    },
+    {
+      "id": 1004,
+      "title": "Cat House",
+      "description": "A place for your cat to play!",
+      "price": 7.99,
+      "image": "images/4.jpg",
+      "availableInventory": 11,
+      "rating": 5
+    },
+    {
+      "id": 1005,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "images/5.jpg",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]
+
+
+  var fluffyApp = new Vue({
+      el: "#fluffy",
+
+  });
